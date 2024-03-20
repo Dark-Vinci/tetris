d5aafa777b6a4788ec5c2c78b0b2d1cba82b2edf
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-03-20 05:14:18 +0000

    chore: more on the admin

--- admins/src/Components/UserNotes/UserNotes.tsx
+++ admins/src/Components/UserNotes/UserNotes.tsx
@@ -1,9 +1,32 @@
 import { JSX } from 'react';
 
-export function UserNotes(): JSX.Element {
+import style from './UserNotes.module.scss';
+
+interface userNotesProps {
+  readonly notes: object[];
+}
+
+export function UserNotes({ notes }: userNotesProps): JSX.Element {
   return (
-    <div>
-      <div> USER DETAILS</div>
+    <div className={style.container}>
+      <table>
+        <thead>
+        <tr>
+          <th>TITLE</th>
+          <th>CREATED AT</th>
+          <th>CONTENT</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        {
+          notes.map(({}, i) => {
+            return <tr key={i}>
+              <td></td>
+            </tr>})
+        }
+        </tbody>
+      </table>
     </div>
   );
 }
