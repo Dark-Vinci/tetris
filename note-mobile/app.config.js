56ba5aa754a0a254c2d2de1545f8cd8e624842e2
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-03-22 14:17:26 +0000

    chore: finally;

--- note-mobile/app.config.js
+++ note-mobile/app.config.js
@@ -1,5 +1,10 @@
 export default {
   extra: {
+    eas: {
+      projectId: 'b6978726-38d0-49d4-a3aa-faad0adaa8f7',
+    },
     baseURL: 'https://tetris-1b7r.onrender.com/notes/api',
   },
+  owner: 'voidi',
+  slug: 'notes',
 };
